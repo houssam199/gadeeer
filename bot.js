9bb6c906ec959e66eb1d02e129c775a7d34aacb4
houssam199 <40376744+houssam199@users.noreply.github.com>
2018-07-31 06:27:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Part of my feelings “Our Relationship”`,"http://twitch.tv/S-F")
+client.user.setGame(`NOTHING`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
